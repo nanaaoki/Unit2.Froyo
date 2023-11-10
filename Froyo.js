912255f4d245c2d1e7780cf5d18c72f1e85c531b
nanaaoki <aoki.nana@gmail.com>
2023-11-09 20:20:37 -0500

    completed workshop

---
+++ Froyo.js
@@ -0,0 +1,22 @@
+var input = prompt("Please input your flavor order(s) here, separated by commas.")
+var array = input.split(", ");
+
+console.log(array);
+
+let flavors = array;
+  
+
+function mapOrder(flavors) {
+    const map = {};
+    for (let i = 0; i < flavors.length; i++) {
+        if (map[flavors[i]]) {
+            map[flavors[i]]++;
+        } else {
+            map[flavors[i]] = 1;
+        }
+    }
+    return map;
+}
+
+console.log("mapOrder result: ", mapOrder(flavors));
+
